97b888725496ee5cdf864ea1568dbdb5417343c5
Matias Montaño <matiasmontano@hotmail.com>
2021-10-03 23:14:41 -0300

    tarea login y logout

---
+++ routes/admin/novedades.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var router = express.Router();
+
+
+/* GET home page. */
+router.get('/', function (req, res, next) {
+  res.render('admin/novedades', {
+    layout: 'admin/layout',
+    usuario: req.session.nombre
+  }); //admin.login.hbs
+});
+
+
+module.exports = router;
+
